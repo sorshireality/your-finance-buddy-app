e333d3fe1afe636ee07e208dae2baf444182cd1c
sorshireality <tutunikssa@gmail.com>
2022-09-25 19:37:10 +0300

    * Mini-error resolve

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import {InjectModel} from '@nestjs/mongoose';
-import {Finance, FinanceDocument} from '../financies/schemas/finance.schema';
 import {Model} from 'mongoose';
 import {User, UserDocument} from './schemas/user.schema';
 
